4f2ac95f0b844f5ad2aedebb3bdf87effd620468
Gamingpig <jonasjohnston6@gmail.com>
2026-08-29 16:35:55 +0200

    fix(status): resolve push notification button click handler, support permission toggling, and improve mobile browser fallback

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // - Sofortige Übernahme: self.skipWaiting() & clients.claim()
 // ==============================================================================
 
-const SW_VERSION = "24.132.2";
+const SW_VERSION = "24.132.3";
 const CACHE_NAME = `gamingpig-cache-v${SW_VERSION}`;
 const CACHE_PREFIX = "gamingpig-cache-";
 
