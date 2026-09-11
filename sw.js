86f1968d9723cda7dfc75c609e97976092ce158d
Gamingpig <gamingpig.support@gmail.com>
2026-09-11 16:33:50 +0200

    fix(ui): fix close button shift bug, isolate community-glass from home, add spring animations to footer menu

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 // - Sofortige Übernahme: self.skipWaiting() & clients.claim()
 // ==============================================================================
 
-const SW_VERSION = "24.160.0";
+const SW_VERSION = "24.161.0";
 const CURRENT_CACHE_VERSION = `gamingpig-cache-v${SW_VERSION}`;
 const CACHE_NAME = CURRENT_CACHE_VERSION;
 
